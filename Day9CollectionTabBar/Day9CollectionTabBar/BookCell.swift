9645c927729e6668f9681a285113b986bc6c7900
verma8986 <35268279+verma8986@users.noreply.github.com>
2018-03-11 17:53:37 -0400

    Add files via upload

---
+++ Day9CollectionTabBar/Day9CollectionTabBar/BookCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookCell.swift
+//  day 9
+//
+//  Created by Jigisha Patel on 2018-03-02.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UICollectionViewCell {
+    
+    @IBOutlet var lblBookTitle: UILabel!
+    @IBOutlet var imgBook: UIImageView!
+}
